4a31c5ad861f6f53d50ad32c97dba29a42b58382
Hyeong Joon Kim <hjoon.kim@bridgelabs.io>
2020-12-02 10:47:38 +0900

    _stat["year-gender"] --> _stat[year][m/f]

--- stat.py
+++ stat.py
@@ -51,17 +51,25 @@ def stat(fname):
 
     _stat = {}
     for item in datalist:
-        key = "%d-%s" % (item["year"], item["gender"])
+        key = item["year"]
+
         if key not in _stat:
             _stat[key] = {
-                "sum":0,
-                "cnt":0,
+                "m":{
+                    "sum":0,
+                    "cnt":0,
+                },
+                "f":{
+                    "sum":0,
+                    "cnt":0,
+                }
             }
 
-        _stat[key]["sum"] += item["score"]
-        _stat[key]["cnt"] += 1
+        gen = item["gender"]
+        _stat[key][gen]["sum"] += item["score"]
+        _stat[key][gen]["cnt"] += 1
 
-    print(json.dumps(_stat, indent=2))
+    #print(json.dumps(_stat, indent=2))
     open("tmp.json", "w").write(json.dumps(_stat, indent=4))
 
     #print(_sum)
@@ -70,23 +78,32 @@ def stat(fname):
     _merge = {}
     for key in _stat:
         #print("key:%d, val:%d" % (key, _sum[key]))
-        avg = _stat[key]["sum"] / _stat[key]["cnt"]
+        avg_m = _stat[key]["m"]["sum"] / _stat[key]["m"]["cnt"]
+        avg_f = _stat[key]["f"]["sum"] / _stat[key]["f"]["cnt"]
 
         _merge[key] = {
-            "year":key,
-            "sum":_stat[key]["sum"],
-            "cnt":_stat[key]["cnt"],
-            "avg":avg
+            "m":{
+                "year":key,
+                "sum":_stat[key]["m"]["sum"],
+                "cnt":_stat[key]["m"]["cnt"],
+                "avg":avg_m
+            },
+            "f":{
+                "year":key,
+                "sum":_stat[key]["f"]["sum"],
+                "cnt":_stat[key]["f"]["cnt"],
+                "avg":avg_f
+            }
         }
 
     open("out.json", "w").write(json.dumps(_merge, indent=4))
     #print(_merge)
     #print("sum[%d]:%d, avg:%.2f, var:%.2f, dev:%.2f" % (count, _sum, avg, var, dev))
 
-    print("YEAR\tAVG(SCORE)")
+    print("YEAR\tM-AVG(SCORE)\tF-AVG(SCORE)")
     keys = sorted(_merge.keys(), reverse=True)
     for key in keys:
-        print("%s\t%f" % (_merge[key]["year"], _merge[key]["avg"]))
+        print("%d\t%f\t%f" % (_merge[key]["f"]["year"], _merge[key]["m"]["avg"], _merge[key]["f"]["avg"]))
 
 def main():
     flag = 1
